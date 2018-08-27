9d3448ffe648c451e4460fdf118b6c275ecbb529
IGoonyI <danyalsyedhameed@gmail.com>
2018-08-26 22:43:45 -0400

    Tree start

---
+++ src/main/java/com/DarwinsNatura/common/init/DarwinsNaturaItems.java
@@ -0,0 +1,10 @@
+package com.DarwinsNatura.common.init;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+import net.minecraft.item.Item;
+
+public class DarwinsNaturaItems {
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+}
