773a350e1ba525bc84bf93d24eb7c3af77c41067
IGoonyI <danyalsyedhameed@gmail.com>
2018-08-29 11:50:05 -0400

    hm

--- build/sources/main/java/com/DarwinsNatura/common/init/DarwinsNaturaItems.java
+++
@@ -1,9 +0,0 @@
-package com.DarwinsNatura.common.init;
-
-import java.util.ArrayList;
-
-import net.minecraft.item.Item;
-
-public class DarwinsNaturaItems {
-	public static final ArrayList<Item> ITEMS = new ArrayList<Item>();
-}
