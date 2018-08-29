765f02740b85b9421ad226bb8e0d05e159329f5e
Iker Fernández <ikerfer002@gmail.com>
2018-08-29 19:33:39 +0200

    m

--- src/main/java/com/DarwinsNatura/core/proxy/ClientProxy.java
+++ src/main/java/com/DarwinsNatura/core/proxy/ClientProxy.java
@@ -10,10 +10,6 @@ import com.DarwinsNatura.common.init.DarwinsNaturaItems;
 import com.DarwinsNatura.common.util.handlers.RegistryHandler;
 import com.DarwinsNatura.core.Reference;
 
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
