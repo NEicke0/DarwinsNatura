7d7a2ace095714d1c0bf836b4077d759103b7b18
SmellyModder <smellysocks543@gmail.com>
2018-08-31 22:26:06 -0400

    Added new Door for Scalesia

--- src/main/java/com/DarwinsNatura/common/init/DarwinsNaturaBlocks.java
+++ src/main/java/com/DarwinsNatura/common/init/DarwinsNaturaBlocks.java
@@ -3,6 +3,7 @@ package com.DarwinsNatura.common.init;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.DarwinsNatura.common.blocks.BlockDoorBase;
 import com.DarwinsNatura.common.blocks.BlockGalapagosFernTree;
 import com.DarwinsNatura.common.blocks.BlockLavaMorningGlory;
 import com.DarwinsNatura.common.blocks.BlockLeavesScalesia;
@@ -12,6 +13,7 @@ import com.DarwinsNatura.common.blocks.BlockPlantBase;
 import com.DarwinsNatura.common.blocks.BlockSaplingScalesia;
 import com.DarwinsNatura.common.blocks.BlockVolcanicRock;
 import com.DarwinsNatura.common.blocks.common.BlockSandBase;
+import com.DarwinsNatura.common.blocks.item.ItemBlockDoor;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBush;
@@ -29,6 +31,9 @@ public class DarwinsNaturaBlocks {
 	  
 	public static final BlockLog SCALESIA_LOG = new BlockLogBase("scalesia_log");
 	public static final Block SCALESIA_PLANK = new BlockPlanksBase("scalesia_plank");
+	public static final Block SCALESIA_DOOR = new BlockDoorBase("scalesia_door");
+	public static final ItemBlockDoor SCALESIA_DOOR_ITEM = new ItemBlockDoor(SCALESIA_DOOR);
+	
 	public static final BlockLeaves SCALESIA_LEAVES = new BlockLeavesScalesia("scalesia_leaves");
 	public static final BlockBush SCALESIA_SAPLING = new BlockSaplingScalesia("scalesia_sapling");
 	public static final BlockBush MICONIA_SHRUB = new BlockPlantBase("miconia_shrub");
